0e5d9a4bc750627287b8cf3bde9b4d60ce858d66
dustij <dev.dusti@gmail.com>
2025-09-23 10:51:32 -0500

    simple py script

---
+++ src/inscight/python/read_zarr.py
@@ -0,0 +1,8 @@
+import zarr
+# Read zarr data from data.zr
+store = zarr.open('data.zr', mode='r')
+print(f"Zarr store keys: {list(store.keys())}")
+# Print basic info about each array
+for key in store.keys():
+    array = store[key]
+    print(f"{key}: shape={array.shape}, dtype={array.dtype}")
